5145e376052cb116065d2d15a58be2cd4c323446
duro06 <andi.meka.025@gmail.com>
2020-02-25 06:52:46 +0700

    inisialisasi produksi

--- src/components/produksi/ProduksiCard.vue
+++ src/components/produksi/ProduksiCard.vue
@@ -1,10 +1,12 @@
 <template>
   <div class="produksi">
-    <Produksi :data="item" />
+    <Produksi :data="items" />
   </div>
 </template>
 <script>
 import Produksi from "./ProduksiEl.vue";
+import * as itemService from "../../services/item_services.js";
+
 export default {
   name: "produksi",
   components: {
@@ -12,10 +14,39 @@ export default {
   },
   data() {
     return {
-      item: {
-        name: "andi"
-      }
+      items: {}
     };
+  },
+  created() {
+    this.req();
+  },
+  methods: {
+    req: async function() {
+      let current_page = this.search == "" ? this.current_page : 1;
+      let sorting = this.sortByDesc ? "DESC" : "ASC";
+      let params = {
+        params: {
+          page: current_page,
+          per_page: this.per_page,
+          q: this.search,
+          sortby: this.sortBy,
+          sortbydesc: sorting
+        }
+      };
+      try {
+        const response = await itemService.loadData(params);
+        console.log(response);
+        let getData = response.data.data;
+        this.items = getData.data;
+        console.log(this.items);
+      } catch (error) {
+        console.log("" + error);
+        this.flashMessage.error({
+          message: "" + error,
+          time: 5000
+        });
+      }
+    }
   }
 };
 </script>
